1f417aa8087fa84f672e236d2e8501ea9a8e2877
SARVANI PMS <santhoshsamineni@gmail.com>
2026-02-13 10:52:02 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
-import os
 from fastapi import FastAPI
-from sqlalchemy import create_engine, text
-from dotenv import load_dotenv
-
-load_dotenv()
+from db.session import engine
+from db.base import Base
 
 app = FastAPI(
     title="KALYANTRA API",
@@ -11,22 +8,15 @@ app = FastAPI(
     version="1.0.0"
 )
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# Create tables automatically
+Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 def root():
     return {"message": "KALYANTRA API is running successfully 🚀"}
 
+
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
-
-@app.get("/db-check")
-def db_check():
-    try:
-        engine = create_engine(DATABASE_URL)
-        with engine.connect() as connection:
-            connection.execute(text("SELECT 1"))
-        return {"database_status": "connected"}
-    except Exception as e:
-        return {"database_status": "error", "details": str(e)}
