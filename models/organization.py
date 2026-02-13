8ff52c47492db870cc0fdff9639ba42d13a8f0ee
SARVANI PMS <santhoshsamineni@gmail.com>
2026-02-13 10:49:16 +0530

    Add Organization model with attributes

--- models/organization.py
+++ models/organization.py
@@ -1,1 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from db.base import Base
+
+
+class Organization(Base):
+    __tablename__ = "organizations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    industry = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
